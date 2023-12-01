f8c8abafbd2e997f41ffbcf87375f71b2541a52a
Joel Felsinger <jfelsinger@spigot.com>
2023-12-01 16:14:58 -0500

    Can't get faster

--- day/1/solution.ts
+++ day/1/solution.ts
@@ -1,19 +1,18 @@
 export default function dayOne(input: string) {
     const wordDigit = /(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)|([0-9])|(\n)/g;
-    let total = 0;
-    let lastDigit = 0;
+    let total = 0, lastDigit = 0;
 
     let out;
     while ((out = wordDigit.exec(input)) !== null) {
-        const match = out[0];
-
         if (out[11]) {
             total += lastDigit;
             lastDigit = 0;
             continue;
         }
 
+        const match = out[0];
         const digit = match[1] ? out.indexOf(match, 1) : +match;
+
         if (match[1]) wordDigit.lastIndex -= 1;
 
         if (!lastDigit) total += digit * 10;
