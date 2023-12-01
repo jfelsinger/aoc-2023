023db9ece7d5b4f7abdac20d85aaf465727762c1
Joel Felsinger <jfelsinger@spigot.com>
2023-12-01 14:50:36 -0500

    Make helper for running solutions

--- day/1/solution.ts
+++ day/1/solution.ts
@@ -1,25 +1,25 @@
-import input from './input.txt';
-const wordDigit = /([0-9])|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)|(\n)/g;
+export default function dayOne(input: string) {
+    const wordDigit = /([0-9])|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)|(\n)/g;
+    let total = 0;
+    let lastDigit = 0;
 
-let total = 0;
-let lastDigit = 0;
+    let out;
+    while ((out = wordDigit.exec(input)) !== null) {
+        const match = out[0];
 
-let out;
-while ((out = wordDigit.exec(input)) !== null) {
-    const match = out[0];
+        if (match === '\n') {
+            total += lastDigit;
+            lastDigit = 0;
+            continue;
+        }
 
-    if (match === '\n') {
-        total += lastDigit;
-        lastDigit = 0;
-        continue;
+        const digit = isNaN(match) ? out.slice(1).indexOf(match) : +match;
+
+        if (match.length > 1) wordDigit.lastIndex -= match.length - 1;
+
+        if (!lastDigit) total += digit * 10;
+        lastDigit = digit;
     }
 
-    const digit = isNaN(match) ? out.slice(1).indexOf(match) : +match;
-
-    if (match.length > 1) wordDigit.lastIndex -= match.length - 1;
-
-    if (!lastDigit) total += digit * 10;
-    lastDigit = digit;
+    return total;
 }
-
-console.log(`total: ${total}`);
--- index.ts
+++ index.ts
@@ -1,1 +1,33 @@
-console.log("Hello via Bun!");
+const day = +(process.env.DAY || (new Date()).getDate());
+
+console.log(`AoC, Day ${day}`);
+const inputFile = Bun.file(`day/${day}/input.txt`);
+const input = await inputFile.text();
+
+const { default: solutionFunc } = await import(`./day/${day}/solution.ts`);
+
+function runSolution() {
+    const start = performance.now() * 1000;
+    const result = solutionFunc(input);
+    return {
+        result,
+        runtime: performance.now()*1000 - start,
+    }
+}
+
+const run = runSolution();
+if (process.env.RUNS && !isNaN(process.env.RUNS)) {
+    let runCount = +(process.env.RUNS);
+
+    const runs: ReturnType<typeof runSolution>[] = [];
+    while(runCount--) {
+        runs.push(runSolution());
+    }
+
+    const avgRuntime = runs.reduce((acc, curr) => acc + curr.runtime, 0) / runs.length / 1000;
+    console.log(`Ran solution ${runs.length} times, at an average of ${avgRuntime.toFixed(2)}ms per run.`);
+    console.log(`Got result: ${run.result}`);
+}
+else {
+    console.log(`Got result: ${run.result} - (in ${(run.runtime / 1000).toFixed(2)}ms)`);
+}
